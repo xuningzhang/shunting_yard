a3dba834a0cc33d77678d1f457fbf6340576885f
XuningZhang <xuningzhang1357@gmail.com>
2026-02-20 18:09:55 -0500

    Correction à la fonction "tokenize" - 20-02-2026
        Des modifications ont été apportées afin de
        corriger le problème suivant :
        - Arrêt du programme lorsque le premier
          élément est négatif.

--- shunting_yard.py
+++ shunting_yard.py
@@ -1,20 +1,28 @@
 def tokenize(expression: str) -> list[str]:
     expression = expression.replace(" ","")
     return_list = []
+    operator_counter = 1        #track how many operator is before a number.
     for element in expression:
         match element:
-            case "+" | "-" | "/" | "*" | "(" | ")":
+            case "+" | "-" | "/" | "*" | "(" :
                 return_list.append(element)
-            case _:
+                operator_counter += 1
+            case ")" :
+                return_list.append(element)
+                operator_counter = 0
+            case _: #if not an operator
                 if len(return_list) == 0:
                     return_list.append(element)
                 else:
-                    if return_list[-1] == "-":
-                        match return_list[-2]:
-                            case "+" | "-" | "/" | "*" | "(" | ")":
+                    match return_list[-1]:
+                        case "+" | "/" | "*" | "(" | ")":
+                            return_list.append(element)
+                        case _:    
+                            if return_list[-1][0] == "-" and operator_counter > 1:
                                 return_list[-1] += element
-                            case _:
+                            elif operator_counter == 0:
+                                return_list[-1] += element
+                            else:
                                 return_list.append(element)
-                    else:
-                        return_list.append(element)
+                operator_counter = 0 #reset to zero if the last added element isn't an operator.
     return return_list
