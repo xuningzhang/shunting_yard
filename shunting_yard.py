10b481ee48d2732b97c82445ac2770191f51f934
XuningZhang <xuningzhang1357@gmail.com>
2026-03-01 09:11:33 -0500

    Définition de la fonction "infix_to_postfix"
    Ajout de l'élément suivant:
        - la fonction "infix_to_postfix"

--- shunting_yard.py
+++ shunting_yard.py
@@ -14,15 +14,49 @@ def tokenize(expression: str) -> list[str]:
                 if len(return_list) == 0:
                     return_list.append(element)
                 else:
-                    match return_list[-1]:
+                    match return_list[-1][0]:
                         case "+" | "/" | "*" | "(" | ")":
                             return_list.append(element)
-                        case _:    
-                            if return_list[-1][0] == "-" and operator_counter > 1:
+                        case "-":
+                            if operator_counter > 1:
                                 return_list[-1] += element
-                            elif operator_counter == 0:
+                            else:
+                                return_list.append(element)
+                        case _:
+                            if operator_counter == 0: #if the last added element is a number:
                                 return_list[-1] += element
                             else:
                                 return_list.append(element)
                 operator_counter = 0 #reset to zero if the last added element isn't an operator.
     return return_list
+
+def infix_to_postfix(tokens: list[str]) -> list[str]:
+    cache = []              #temporary operator stack
+    posfixed_tokens = []     #return list
+    for element in tokens:
+        match element:
+            case "-" | "+" :
+                priority = "-+*/"
+            case "*" | "/" :
+                priority = "*/"
+        match element:
+            case "(" :
+                cache.append(element)
+            case ")" :
+                while cache[-1] != "(":
+                    posfixed_tokens.append(cache.pop())
+                del cache[-1]
+            case "+" | "-" | "/" | "*" :
+                condition = True
+                while condition:
+                    if len(cache) <= 0:
+                        condition = False
+                    elif cache[-1] in priority:
+                        posfixed_tokens.append(cache.pop())
+                    else:
+                        condition = False
+                cache.append(element)
+            case _:
+                posfixed_tokens.append(element)
+    posfixed_tokens.extend(reversed(cache))
+    return posfixed_tokens
